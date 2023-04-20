40bee39f46e4ee73e78d8c4b8053f451d42301a4
stepa00 <stttep@gmail.com>
2023-04-20 18:12:28 +0300

    Created main routes for the application.

--- app.py
+++ app.py
@@ -7,15 +7,18 @@ import sqlite3
 
 # Configure application
 app = Flask(__name__)
+app.debug = True
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+
 # Configure sqlite3 database
 connection = sqlite3.connect("calendars.db")
 cursor = connection.cursor()
+
 
 # TODO: something about cashing. See if can function without it.
 @app.after_request
@@ -26,8 +29,48 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
+
 @app.route("/")
 # @login_required
 def index():
     # TODO: create form for the event
     return render_template("index.html")
+
+
+@app.route("/login")
+def login():
+    # TODO: create a login page
+    return render_template("login.html")
+
+
+@app.route("/register")
+def register():
+    # TODO: create a register page
+    return render_template("register.html")
+
+
+@app.route("/history")
+def history():
+    # TODO: create history of events 
+    # make a button to recreaet an old event
+    return render_template("history.html")
+
+
+@app.route("/settings")
+def settings():
+    # TODO: create settings with password change
+    # and deleting the history of events
+    return render_template("settings.html")
+
+
+@app.route("/logout")
+def logout():
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run()
--- helpers.py
+++ helpers.py
@@ -19,18 +19,3 @@ def login_required(f):
         return f(*args, **kwargs)
     return decorated_function
 
-
-# Output mems with 400 problems code
-def apology(message, code=400):
-    """Render message as an apology to user."""
-    def escape(s):
-        """
-        Escape special characters.
-
-        https://github.com/jacebrowning/memegen#special-characters
-        """
-        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
-                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
-            s = s.replace(old, new)
-        return s
-    return render_template("apology.html", top=code, bottom=escape(message)), code
